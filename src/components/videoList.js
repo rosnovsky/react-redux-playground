695eeb6b344456988ae73c1f90c9c578a54e6dfe
Artem Rosnovskiy <artem@rosnovsky.us>
2017-08-25 21:57:44 -0700

    Basic React app complete

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -6,7 +6,10 @@ const VideoList = (props) => {
     
     const videoItems = props.videos.map((video) => {
          return (
-            <VideoListItem key={video.etag} video={video} />
+            <VideoListItem 
+                onVideoSelect={props.onVideoSelect}
+                key={video.etag}
+                video={video} />
          )
         })
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -9,19 +10,32 @@ const API_KEY = 'AIzaSyDdAuu5kA-Lrl2LVZSiZ7Zy6eb7mcP9E2E';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: []};
         
-        YTSearch({ key: API_KEY, term: "Total Solar Eclipse 2017"}, (videos) => {
-                this.setState({ videos });
-            });
-        }
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch("linkin park");
+    }
+        
+        videoSearch(term) {
+            YTSearch({ key: API_KEY, term}, (videos) => {
+                this.setState({ 
+                    videos,
+                    selectedVideo: videos[0] });
+                });
+            }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
       return(
         <div>
-            <SearchBar />
-            <VideoDetail video = {this.state.videos[0] }/>
-            <VideoList videos={this.state.videos} />
+            <SearchBar onSearchTermChange = {videoSearch} />
+            <VideoDetail video = {this.state.selectedVideo }/>
+            <VideoList 
+                onVideoSelect = { selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
         </div>
       )}};
 
