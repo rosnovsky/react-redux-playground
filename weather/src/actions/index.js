9afa94214a68d83e7b4ac74b2c84791321f5a9cc
Artem Rosnovskiy <artem@rosnovsky.us>
2017-08-30 18:12:20 -0700

    🔐 HTTPSed OpenWeather request 🔐

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${keys.API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${keys.API_KEY}`;
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city}`;
